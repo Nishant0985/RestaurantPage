da4fbe7e73cf0d7308ccc3e2c66819882c829020
Nishant0985 <nishantp2468@gmail.com>
2023-05-25 21:25:06 +0530

    My Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Restorent from "./component/Basic/Restorent";
+
+const App = () => {
+  return (
+    <>
+      <Restorent />
+    </>
+  );
+};
+
+export default App;
